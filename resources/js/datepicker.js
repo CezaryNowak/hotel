4545a806b386fd95abeb999ddf9bcd075c4643f0
Cesar <nowak.cezary990@gmail.com>
2024-01-18 15:29:11 +0100

    Booking with confirm

--- resources/js/datepicker.js
+++ resources/js/datepicker.js
@@ -5,7 +5,6 @@ export function datePicker(excludedDates) {
     const input = document.getElementById('input-id');
     const today = new Date();
     today.setDate(today.getDate() + 1);
-    console.log(excludedDates);
 
     const options = {
         format: 'DD-MM-YYYY',
